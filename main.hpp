dfca885d131b48739d9123688636b88c838fa037
Skylar Au-Juarez <84100709+SkylarAJ@users.noreply.github.com>
2024-04-02 09:54:33 -0700

    main.hpp

--- main.hpp
+++ main.hpp
@@ -8,6 +8,27 @@ const int MAXLEN = 100;
 
 int splitline(string, char, char[][MAXLEN]);
 
-/*******************************
- * Code your program here
- *******************************/
+int splitline(string txt, char delimiter, char target[][MAXLEN]) {
+    // Initialize variables
+    int cnt = 0;
+    int wordIndex = 0;
+
+    // Iterate through each character in the string
+    for (int i = 0; i < txt.length(); ++i) {
+        // If the character is the delimiter, push the current word into the target array
+        if (txt[i] == delimiter) {
+            txt.copy(target[cnt], i - wordIndex, wordIndex);
+            target[cnt][i - wordIndex] = '\0'; // Null-terminate the string
+            wordIndex = i + 1;
+            cnt++;
+        }
+    }
+    
+    // Push the last word into the target array
+    txt.copy(target[cnt], txt.length() - wordIndex, wordIndex);
+    target[cnt][txt.length() - wordIndex] = '\0'; // Null-terminate the string
+    cnt++;
+
+    // Return the number of split words
+    return cnt;
+}
